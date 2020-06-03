580a42f21a6f4056dc3e338bfbd74b713af895cd
Mironov A <Licomail256@gmail.com>
2020-06-03 18:53:37 +0300

    Add 12 buttons that don`t actually display as intended

---
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.makentoshe.androidgithubcitemplate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.set_game_2_players)
+
+    }
+}
